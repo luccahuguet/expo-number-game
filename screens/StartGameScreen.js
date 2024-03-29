729a42a0e659676e3d2d31b41e7f12bfc00c76bb
luccahuguet <lucca.huguet2@gmail.com>
2024-03-29 10:57:53 -0300

    better buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,14 +7,22 @@ const StartGameScreen = (props) => {
     <View style={styles.container}>
       <Text style={styles.text}>Numbers Game!{props.name}</Text>
       <TextInput style={styles.numberInput} maxLength={2} keyboardAppearance="dark" keyboardType="number-pad" autoCapitalize="none" autoCorrect={false} />
-      <PrimaryButton> Reset </PrimaryButton>
-      <PrimaryButton> Confirm</PrimaryButton>
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton> Reset </PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton> Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
@@ -26,6 +34,15 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 8,
+  },
+  buttonContainer: {
+    flex: 1,
   },
   text: {
     fontSize: 20,
